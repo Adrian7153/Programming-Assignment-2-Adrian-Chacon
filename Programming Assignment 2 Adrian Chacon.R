9535612d89741e72fc009d9489c1820b83191c6c
Adrian7153 <75229566+Adrian7153@users.noreply.github.com>
2020-11-29 20:31:32 -0500

    Add files via upload

---
+++ Programming Assignment 2 Adrian Chacon.R
@@ -0,0 +1,32 @@
+## Overall Description:
+
+## I am trying to compute the inverse of a mtrix with
+## some practical functions
+
+## We use two functions to solve this hard problem in
+## in a easy way
+
+
+## makeCacheMatrix: This function creates a special 
+## "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  matrix1 <- NULL
+  xxxx <- function (x){
+    inv <- function(x)
+    inv(matrix1) <- theinv
+  }  
+}
+
+
+## This function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If 
+## the inverse has already been calculated (and the 
+## matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  returnmatrix <- function(theinv){
+    return(theinv) <- newmatr
+  }
+}
